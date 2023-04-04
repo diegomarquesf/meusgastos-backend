3aa0223c3fd685ebd87a5fb3cd4acb08e849afd2
Diego Marques <fariia_di@hotmail.com>
2023-04-04 16:00:40 -0300

    Projeto criado

---
+++ src/test/java/br/com/diegomarques/meusgastos/MeusgastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diegomarques.meusgastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusgastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
